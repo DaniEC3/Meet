e4ec14f5a504cf49b0c31ae783d2d55a240d1e50
DaniEC3 <167336437+DaniEC3@users.noreply.github.com>
2025-03-25 18:06:25 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
+atatus.notify(new Error('Test Atatus Setup'));
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import * as atatus from 'atatus-spa';
+
+
 
 atatus.config('1a71fb9e37be4086843358fb2e253574').install();
 
