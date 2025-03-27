a962e57ef65a443ce04354b662eee56f86a4308e
DaniEC3 <167336437+DaniEC3@users.noreply.github.com>
2025-03-27 15:53:14 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import App from './App.jsx';
 import './index.css';
 import * as atatus from 'atatus-spa';
 
-atatus.notify(new Error('Test Atatus Setup'));
-
 atatus.config('1a71fb9e37be4086843358fb2e253574').install();
+
+atatus.notify(new Error('Test Atatus Setup'));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
