4bd78b87e5985ca55379baa8fcecdb0ef19d83d6
DaniEC3 <167336437+DaniEC3@users.noreply.github.com>
2025-03-29 16:40:45 -0400

    manifest and config added

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+// import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import * as atatus from 'atatus-spa';
 
-atatus.config('1a71fb9e37be4086843358fb2e253574').install();
+// atatus.config('1b1ee5665eb943e3bdb71259beabe47a').install();
 
-// atatus.notify(new Error('Test Atatus Setup'));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ <React.StrictMode>
+   <App />
+ </React.StrictMode>,
+);
+serviceWorkerRegistration.register();
