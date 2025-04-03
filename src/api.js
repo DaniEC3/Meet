f9392cf663e675177b2da23fb96e4a97249f3075
DaniEC3 <167336437+DaniEC3@users.noreply.github.com>
2025-04-03 18:48:48 -0400

    nprogress deleted

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import mockData from './mock-data';
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
+
+
 /**
  *
  * @param {*} events:
@@ -21,13 +21,11 @@ export const extractLocations = (events) => {
  * This function will fetch the list of all events
  */
 export const getEvents = async () => {
-  NProgress.start();
   if (window.location.href.startsWith('http://localhost')) {
     return mockData;
   }
   if (!navigator.onLine) {
     const events = localStorage.getItem("lastEvents");
-    NProgress.done();
     return events ? JSON.parse(events) : [];
   }
   const token = await getAccessToken();
@@ -52,7 +50,6 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
-      NProgress.done();
       localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
